6fa7de0f693ec8dd7d7e3d1b93a2fcb4a47d570c
Enyon ADJANOR <enyonadjanor@gmail.com>
2021-05-07 11:20:46 +0200

    CercleFactory

--- src/fr/diginamic/essais/TestCercle.java
+++ src/fr/diginamic/essais/TestCercle.java
@@ -1,18 +1,21 @@
 package fr.diginamic.essais;
 
 import fr.diginamic.entities.Cercle;
+import fr.diginamic.utils.CercleFactory;
 
 public class TestCercle {
 
     public static void main(String[] args) {
 
-        Cercle cercle = new Cercle(4.2);
-        Cercle cercle1 = new Cercle(3.7);
+        Cercle cercle = CercleFactory.fabriquerCercle(4.2);
+        Cercle cercle1 = CercleFactory.fabriquerCercle(3.7);
+
 
         System.out.println("Le perimètre du premier cercle est : " + cercle.perimetreCercle());
         System.out.println("La surface du premier cercle est : " + cercle.surfaceCercle());
-        System.out.println("***************************");
+
         System.out.println("Le perimètre du second cercle est : " + cercle1.perimetreCercle());
         System.out.println("La surface du second cercle est : " + cercle1.surfaceCercle());
+
     }
 }
